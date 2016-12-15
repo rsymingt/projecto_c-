4085902b923c80ba71997f64c8e970794871e574
rsymingt <rsymingt@mail.uoguelph.ca>
2016-12-15 18:58:59 -0500

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,24 @@
 int main()
 {
 	
-	
+	Tree tree;
+
+	std::string usr_input;
+
+	do{
+
+		std::cin >> usr_input;
+
+		if(usr_input == "insert")
+		{
+			if(int i = usr_input.find(" "))
+			{
+				std::cout << "YAY" << std::endl;
+			}
+			tree.insert(usr_input);
+		}
+
+	}while(usr_input != "q");
+
 	return 0;
 }
